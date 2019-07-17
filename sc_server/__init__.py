8f87e651a0a6d5a687408d7f4a33ae76e3ddb898
Parrot <parrot@localhost.localdomain>
2019-07-17 03:40:29 +0200

    Small fix

--- sc_server/__init__.py
+++ sc_server/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/base.db'
 db = SQLAlchemy(app)
 
-from flaskapp import routes
+from sc_server import routes
