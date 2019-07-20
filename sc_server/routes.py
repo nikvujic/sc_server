326e73cb9f7290f8e95f4e8fbd976458082d03a2
Parrot <parrot@localhost.localdomain>
2019-07-20 02:20:16 +0200

    Added routes for zaposleni

--- sc_server/routes.py
+++ sc_server/routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from sc_server import app, db
-from sc_server.models import Sastojak
+from sc_server.models import Sastojak, Zaposleni
 
 @app.route('/sastojak', methods=['GET'])
 def get_all_sastojci():
@@ -37,12 +37,67 @@ def create_sastojak():
 @app.route('/sastojak/<id>', methods=['DELETE'])
 def delete_sastojak(id):
     sastojak = Sastojak.query.filter_by(id=id).first()
-    naziv = sastojak.naziv
 
     if not sastojak:
-        return jsonify({'poruka':'Sastojak nije pronadjen!'})
+        return jsonify({'poruka':'Sastojak nije pronadjen!'}), 409
+
+    naziv = sastojak.naziv
 
     db.session.delete(sastojak)
     db.session.commit()
 
     return jsonify({'poruka':f'Sastojak <{naziv}> obrisan!'})
+
+@app.route('/zaposleni', methods=['GET'])
+def get_all_zaposleni():
+    lista_zaposlenih = Zaposleni.query.all()
+    zaposleni_to_send = []
+
+    for zaposleni in lista_zaposlenih:
+       current_zaposleni = {}
+       current_zaposleni['ime'] = zaposleni.ime
+       current_zaposleni['prezime'] = zaposleni.prezime
+       current_zaposleni['JMBG'] = zaposleni.JMBG
+       current_zaposleni['admin'] = zaposleni.admin
+       zaposleni_to_send.append(current_zaposleni)
+
+    return jsonify({'zaposleni':zaposleni_to_send})
+
+@app.route('/zaposleni', methods=['POST'])
+def create_zaposleni():
+    data = request.get_json()
+    z_ime = data['ime']
+    z_prezime = data['prezime']
+    z_JMBG = data['JMBG']
+    
+    try:
+        z_admin = data['admin']
+        new_zaposleni = Zaposleni(ime=z_ime, prezime=z_prezime, JMBG=z_JMBG, admin=z_admin) 
+    except:
+        new_zaposleni = Zaposleni(ime=z_ime, prezime=z_prezime, JMBG=z_JMBG)
+
+    lista_zaposlenih = Zaposleni.query.all()
+
+    for zaposleni in lista_zaposlenih:
+        if zaposleni.JMBG == z_JMBG:
+            return jsonify({'poruka':f'Zaposleni vec postoji!'}), 409
+    
+    db.session.add(new_zaposleni)
+    db.session.commit()
+
+    return jsonify({'poruka':f'Zaposleni <{z_ime} {z_prezime}> uspesno kreiran!'})
+
+@app.route('/zaposleni/<JMBG>', methods=['DELETE'])
+def delete_zaposleni(JMBG):
+    zaposleni = Zaposleni.query.filter_by(JMBG=JMBG).first()
+
+    if not zaposleni:
+        return jsonify({'poruka':'Zaposleni nije pronadjen!'}), 409
+
+    ime = zaposleni.ime
+    prezime = zaposleni.prezime
+
+    db.session.delete(zaposleni)
+    db.session.commit()
+
+    return jsonify({'poruka':f'zaposleni <{ime} {prezime}> obrisan!'})
