2482598cbb354133301288b9a4a81f840246e136
Parrot <parrot@localhost.localdomain>
2019-07-29 01:26:15 +0200

    Added secret key for (used for token verification)

--- sc_server/__init__.py
+++ sc_server/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'developement_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/base.db'
 db = SQLAlchemy(app)
 
